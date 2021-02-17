918ef5eb4337ca3d9ea08008ee0a2c1f05a6cd23
taratimmerman <timmermantara@gmail.com>
2021-02-16 21:53:27 -0700

    migrations & seeds done

--- data/migrations/20210216212344_cars_table.js
+++ data/migrations/20210216212344_cars_table.js
@@ -2,8 +2,13 @@ const { table } = require("../dbConfig");
 
 exports.up = function(knex) {
   return knex.schema.createTable('cars', table => {
-    table.increments();
-    table.text('name',128).unique().notNullable();
+      table.increments()
+      table.text('vin',17).unique().notNullable();
+      table.text('make',30);
+      table.text('model',70);
+      table.text('mileage',100000);
+      table.text('transmission',50);
+      table.text('titlestatus',50);
   });
 };
 
